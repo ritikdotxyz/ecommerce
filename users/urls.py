494cb32bff0fa8a9dc0d7907026b26ddec5241f9
ritik <letter.ritik060@gmail.com>
2025-09-25 18:27:16 +0545

    login and singup api

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("signup/", views.signup, name="signup"),
     path("logout/", views.log_out, name="logout"),
+    path("users/", views.user_list, name="user_list"),
+    path("users/<int:id>", views.user_detail, name="user_detail"),
+    path("users/signup/", views.api_signup, name="api_signup"),
+    path("users/login/", views.api_login, name="api_login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,78 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
-from django.http import HttpResponse
 from django.contrib.auth import logout
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import CustomUser
 from .forms import CustomerCreationForm, LoginForm
+from .serializers import CustomUserSerializer
+
+
+@api_view(["POST"])
+def api_login(request):
+    email = request.data["email"]
+    password = request.data["password"]
+    user = authenticate(request, email=email, password=password)
+
+    if user:
+        return Response({"message": "Login Successful"}, status=status.HTTP_200_OK)
+    return Response({"message": "Failed to login"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def api_signup(request):
+    if request.method == "POST":
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "message": "User Successfully created.",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "POST"])
+def user_list(request):
+    if request.method == "GET":
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    if request.method == "POST":
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def user_detail(request, id):
+    try:
+        user = CustomUser.objects.get(id=id)
+    except CustomUser.DoesNotExist:
+        return Response(status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = CustomUserSerializer(user, many=False)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    elif request.method == "PUT":
+        serializer = CustomUserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        user.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
 
 
 def login(request):
