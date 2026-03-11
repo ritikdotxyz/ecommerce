aa9bc30e57b4842f29e07653ad48b85af2b44e0a
ritik <letter.ritik060@gmail.com>
2026-03-11 15:32:15 +0545

    fix same url name

--- users/api/urls.py
+++ users/api/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("login/", views.api_login, name="api_login"),
-    path("signup/", views.api_signup, name="signup"),
-    path("logout", views.api_logout, name="logout"),
-    path("users/", views.user_list, name="users"),
-    path("user-detail/", views.user_detail, name="user-detail"),
+    path("signup/", views.api_signup, name="api_signup"),
+    path("logout", views.api_logout, name="api_logout"),
+    path("users/", views.user_list, name="api_users"),
+    path("user-detail/", views.user_detail, name="api_user_detail"),
 ]
